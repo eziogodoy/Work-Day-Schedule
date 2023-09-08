bda8d37bbee71e48c8e9cb29c305efe376b907c6
Ezio Godoy <eziogodoylopez@gmail.com>
2023-09-08 02:29:47 -0400

    Javascript (in progress)

--- script.js
+++ script.js
@@ -17,3 +17,27 @@ clearCheck(dayofWeek);
 
 
 
+//console.log(today);
+$('#currentDay').text(todayDisplay);
+
+function getStored() {
+    if (Array.isArray(StoredArray)) {
+        dailyPlanner = JSON.parse(localStorage.getItem('plansToday'));
+    } else {
+        return;
+    }
+}
+getStored();
+//console.log(dailyPlanner);
+
+function getContent() {
+    for (var plan = 0; plan < dailyPlanner.length; plan++) {
+        $(`[data-index =${plan}]`).text(dailyPlanner[plan]);
+        //console.log($(`[data-index =${plan}]`));
+        
+    }
+}
+
+
+
+
